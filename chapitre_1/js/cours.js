faf90fe695002c6903507f9b1c7df03eebcbeedd
bpesquet <bpesquet@gmail.com>
2015-07-17 13:02:32 +0200

    Chapitre 1

--- chapitre_1/js/cours.js
+++ chapitre_1/js/cours.js
@@ -1,5 +1,5 @@
 console.log("Bonjour en JavaScript !");
-console.log("Faisons quelques calculs.");
+//console.log("Faisons quelques calculs.");
 console.log(4 + 7);
-console.log(12 / 0);
+//console.log(12 / 0);
 console.log("Au revoir !");
