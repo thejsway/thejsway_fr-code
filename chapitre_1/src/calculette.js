7a0cc78172c1b975e9ea031dc23ab532ad3ca283
bpesquet <bpesquet@gmail.com>
2015-07-17 10:59:54 +0200

    Chapitre 1

---
+++ chapitre_1/src/calculette.js
@@ -0,0 +1,8 @@
+/*
+Exercice : afficher le résultats des opérations mathématiques entre 6 et 3
+*/
+
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(6 / 3);
---
+++ chapitre_1/src/presentation.js
@@ -0,0 +1,6 @@
+/*
+Exercice : écrivez votre nom et votre âge
+*/
+
+console.log("Baptiste");
+console.log(38);
