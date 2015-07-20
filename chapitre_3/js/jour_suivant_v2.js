3a1221beebbef4b443038f76828ef1d7074ae8f6
bpesquet <bpesquet@gmail.com>
2015-07-20 08:55:08 +0200

    Suppression . dans messages

--- chapitre_3/js/jour_suivant_v2.js
+++ chapitre_3/js/jour_suivant_v2.js
@@ -7,25 +7,25 @@ var jour = prompt("Entrez un jour de la semaine :");
 // Solution avec un switch
 switch (jour) {
 case "lundi":
-    console.log("Demain, nous serons mardi.");
+    console.log("Demain, nous serons mardi");
     break;
 case "mardi":
-    console.log("Demain, nous serons mercredi.");
+    console.log("Demain, nous serons mercredi");
     break;
 case "mercredi":
-    console.log("Demain, nous serons jeudi.");
+    console.log("Demain, nous serons jeudi");
     break;
 case "jeudi":
-    console.log("Demain, nous serons vendredi.");
+    console.log("Demain, nous serons vendredi");
     break;
 case "vendredi":
-    console.log("Demain, nous serons samedi.");
+    console.log("Demain, nous serons samedi");
     break;
 case "samedi":
-    console.log("Demain, nous serons dimanche.");
+    console.log("Demain, nous serons dimanche");
     break;
 case "dimanche":
-    console.log("Demain, nous serons lundi.");
+    console.log("Demain, nous serons lundi");
     break;
 default:
     console.log("Jour non reconnu !");
