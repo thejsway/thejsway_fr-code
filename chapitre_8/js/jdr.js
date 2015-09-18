3209b2cf7f9d6951ada86e22024c3768281fcb99
bpesquet <bpesquet@gmail.com>
2015-09-18 19:05:17 +0200

    JdR avec syntaxe ES5

---
+++ chapitre_8/js/jdr.js
@@ -0,0 +1,82 @@
+/*
+Mini-jeu de rôle avec syntaxe ES5
+*/
+
+var Personnage = {
+    nom: "",
+    sante: 0,
+    force: 0,
+
+    initPerso: function (nom, sante, force) {
+        this.nom = nom;
+        this.sante = sante;
+        this.force = force;
+    },
+
+    // Attaque un personnage cible
+    attaquer: function (cible) {
+        if (this.sante > 0) {
+            var degats = this.force;
+            console.log(this.nom + " attaque " + cible.nom + " et lui fait " + degats + " points de dégâts");
+            cible.sante -= degats;
+            if (cible.sante > 0) {
+                console.log(cible.nom + " a encore " + cible.sante + " points de vie");
+            } else {
+                cible.sante = 0;
+                console.log(cible.nom + " est mort !");
+            }
+        } else {
+            console.log(this.nom + " ne peut pas attaquer : il est mort...");
+        }
+    }
+};
+
+var Joueur = Object.create(Personnage);
+Joueur.initJoueur = function (nom, sante, force) {
+    this.initPerso(nom, sante, force);
+    this.xp = 0;
+};
+// Renvoie la description du joueur
+Joueur.decrire = function () {
+    var description = this.nom + " a " + this.sante + " points de vie, " +
+        this.force + " en force et " + this.xp + " points d'expérience";
+    return description;
+};
+Joueur.combattre = function (pnj) {
+    this.attaquer(pnj);
+    if (pnj.sante === 0) {
+        console.log(this.nom + " a tué " + pnj.nom + " et gagne " +
+            pnj.valeur + " points d'expérience");
+        this.xp += pnj.valeur;
+    }
+}
+
+var PNJ = Object.create(Personnage);
+PNJ.initPNJ = function (nom, sante, force, race, valeur) {
+    this.initPerso(nom, sante, force);
+    this.race = race;
+    this.valeur = valeur;
+};
+
+var joueur1 = Object.create(Joueur);
+joueur1.initJoueur("Aurora", 150, 25);
+var joueur2 = Object.create(Joueur);
+joueur2.initJoueur("Glacius", 130, 30);
+
+console.log("Bienvenue dans ce jeu d'aventure ! Voici nos courageux héros :");
+console.log(joueur1.decrire());
+console.log(joueur2.decrire());
+
+var monstre = Object.create(PNJ);
+monstre.initPNJ("ZogZog", 40, 20, "orc", 10);
+
+console.log("Un affreux monstre arrive : c'est un " + monstre.race + " nommé " + monstre.nom);
+
+monstre.attaquer(joueur1);
+monstre.attaquer(joueur2);
+
+joueur1.combattre(monstre);
+joueur2.combattre(monstre);
+
+console.log(joueur1.decrire());
+console.log(joueur2.decrire());
