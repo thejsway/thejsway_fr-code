0fe6d961045d3594e4561db07c8e916e97ce1bb0
bpesquet <bpesquet@gmail.com>
2015-09-18 19:04:56 +0200

    Ajout XP personnage

--- chapitre_7/js/jdr.js
+++ chapitre_7/js/jdr.js
@@ -1,12 +1,17 @@
+/*
+Mini-jeu de rôle
+*/
+
 var perso1 = {
     nom: "Aurora",
     sante: 150,
     force: 25,
+    xp: 0,
 
     // Renvoie la description du personnage
     decrire: function () {
-        var description = this.nom + " a " + this.sante + " points de vie et " +
-            this.force + " en force";
+        var description = this.nom + " a " + this.sante + " points de vie, " +
+            this.force + " en force et " + this.xp + " points d'expérience";
         return description;
     }
 };
@@ -18,5 +23,8 @@ perso1.sante = perso1.sante - 20;
 
 // Aurora trouve un bracelet de force
 perso1.force = perso1.force + 10;
+
+// Aurora apprend une nouvelle compétence
+perso1.xp = perso1.xp + 15;
 
 console.log(perso1.decrire());
