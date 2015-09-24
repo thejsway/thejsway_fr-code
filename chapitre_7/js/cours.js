1f376f391b875737ee9f647e8a415d1579633859
bpesquet <bpesquet@gmail.com>
2015-09-24 08:42:51 +0200

    Ajout modif objet

--- chapitre_7/js/cours.js
+++ chapitre_7/js/cours.js
@@ -13,3 +13,11 @@ console.log(stylo['couleurEncre']); // Affiche "bleu"
 console.log(stylo['marque']); // Affiche "Bic"
 
 console.log("Mon stylo à " + stylo.type + " " + stylo.marque + " écrit en " + stylo.couleurEncre);
+
+stylo.couleurEncre = "rouge"; // Modifie la couleur de l'encre du stylo
+
+console.log("Mon stylo à " + stylo.type + " " + stylo.marque + " écrit en " + stylo.couleurEncre);
+
+stylo.prix = 2.5; // Ajout de la propriété prix à l'objet stylo
+
+console.log("Il coûte " + stylo.prix + " euros");
