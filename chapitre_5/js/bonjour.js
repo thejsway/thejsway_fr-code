18c975ba00e0224ca87f26af3c303be65f756763
bpesquet <bpesquet@gmail.com>
2015-10-05 18:41:17 +0200

    bonjour() -> direBonjour()

--- chapitre_5/js/bonjour.js
+++ chapitre_5/js/bonjour.js
@@ -3,11 +3,11 @@ Exercice : fonction bonjour avec saisies utilisateur
 */
 
 // Renvoie un message de bienvenue
-function bonjour(prenom, nom) {
+function direBonjour(prenom, nom) {
     var message = "Bonjour, " + prenom + " " + nom + " !";
     return message;
 }
 
 var p = prompt("Entrez votre prénom :");
 var n = prompt("Entrez votre nom :");
-console.log(bonjour(p, n));
+console.log(direBonjour(p, n));
