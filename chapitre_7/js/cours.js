8f173bbe823ede4c42463dcef701be9acacc1fdb
bpesquet <bpesquet@gmail.com>
2015-10-07 19:32:38 +0200

    couleurEncre -> couleur

--- chapitre_7/js/cours.js
+++ chapitre_7/js/cours.js
@@ -1,22 +1,22 @@
 var stylo = {
     type: "bille",
-    couleurEncre: "bleu",
+    couleur: "bleu",
     marque: "Bic"
 };
 
 console.log(stylo.type); // Affiche "bille"
-console.log(stylo.couleurEncre); // Affiche "bleu"
+console.log(stylo.couleur); // Affiche "bleu"
 console.log(stylo.marque); // Affiche "Bic"
 
 console.log(stylo['type']); // Affiche "bille"
-console.log(stylo['couleurEncre']); // Affiche "bleu"
+console.log(stylo['couleur']); // Affiche "bleu"
 console.log(stylo['marque']); // Affiche "Bic"
 
-console.log("Mon stylo à " + stylo.type + " " + stylo.marque + " écrit en " + stylo.couleurEncre);
+console.log("Mon stylo à " + stylo.type + " " + stylo.marque + " écrit en " + stylo.couleur);
 
-stylo.couleurEncre = "rouge"; // Modifie la couleur de l'encre du stylo
+stylo.couleur = "rouge"; // Modifie la couleur de l'encre du stylo
 
-console.log("Mon stylo à " + stylo.type + " " + stylo.marque + " écrit en " + stylo.couleurEncre);
+console.log("Mon stylo à " + stylo.type + " " + stylo.marque + " écrit en " + stylo.couleur);
 
 stylo.prix = 2.5; // Ajout de la propriété prix à l'objet stylo
 
