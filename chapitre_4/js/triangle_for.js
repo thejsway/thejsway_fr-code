b563a7d3b85668310974528494afc0eaf2550761
bpesquet <bpesquet@gmail.com>
2015-10-25 14:22:04 +0100

    Précisions sur l'opérateur += avec les chaînes

--- chapitre_4/js/triangle_for.js
+++ chapitre_4/js/triangle_for.js
@@ -4,6 +4,8 @@ Exercice : construire et afficher un triangle de 7 lignes
 
 var ligne = "";
 for (var i = 1; i <= 7; i++) {
-    ligne += "#";
+    ligne = ligne + "#";
+    // Autre possibilité avec l'opérateur +=
+    //ligne += "#";
     console.log(ligne);
 }
--- chapitre_4/js/triangle_while.js
+++ chapitre_4/js/triangle_while.js
@@ -5,7 +5,9 @@ Exercice : construire et afficher un triangle de 7 lignes
 var ligne = "";
 var i = 1;
 while (i <= 7) {
-    ligne += "#";
+    ligne = ligne + "#";
+    // Autre possibilité avec l'opérateur +=
+    //ligne += "#";
     console.log(ligne);
     i++;
 }
